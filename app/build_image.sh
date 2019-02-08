bcda5e850e305661b8a88f972efae18dc6c9c3f5
Marko Mikulicic <mkm@bitnami.com>
2019-02-08 11:24:37 +0100

    Print out how to use the image

--- app/build_image.sh
+++ app/build_image.sh
@@ -3,3 +3,12 @@
 set -euo pipefail
 
 stacksmith build | tee image_spec.json
+
+image=$(jq -r <image_spec.json '.result|.[]|select(.name=="image")|.value')
+
+cat <<EOF
+=====
+You can run the image with:
+
+  docker run -ti -p 8080:80 ${image}
+EOF
